a1ef99a299663585b9fe55b0222d53186fe8ff20
Samir El Hassan <samirelhassann@gmail.com>
2024-02-04 22:12:38 -0300

    ci: create the deploy.yml to deploy the application on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react";
 
-const base = process.env.VITE_BASE || "/";
-
 export default defineConfig({
   plugins: [react()],
-  base,
 });
